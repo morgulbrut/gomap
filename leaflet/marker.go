aa4f4e9b4c7aa3b75d27f442555cde90375b9715
Tillo <tillo.bosshart@gmail.com>
2019-12-05 07:11:16 +0100

    implemented random colors (kinda hacky)

--- leaflet/marker.go
+++ leaflet/marker.go
@@ -2,7 +2,9 @@ package leaflet
 
 import (
 	"bytes"
+	"math/rand"
 	"text/template"
+	"time"
 
 	"github.com/morgulbrut/colorlog"
 )
@@ -41,17 +43,29 @@ var colors = []string{"red", "blue", "green", "purple", "orange", "darkred", "li
 
 // MarkerString returns a chunk of JS representing a marker
 func MarkerString(m Marker, ma Mapdata) []byte {
-
-	marker := `
-	var marker = L.marker([{{.Position.Lat}}, {{.Position.Lon}}],{icon: new L.Icon.Default()}).addTo(marker_cluster);
-	var icon = L.AwesomeMarkers.icon({icon: '{{.Icon.Symbol}}', iconColor: '{{.Icon.IconColor}}', markerColor: '{{.Icon.MarkerColor}}', prefix: 'fa', extraClasses: 'fa-rotate-0'});
-	marker.setIcon(icon);
-  
-	var popup = L.popup({maxWidth: '300'});
-	var html = $('<div id="html" style="width: 100.0%%; height: 100.0%%;"><a target="_blank" href="{{.Popup.Link}}"><b>{{.Popup.Title}}</b></a><br/>{{.Popup.Desc}}<br/><br/><b>{{.Popup.Address}}</b><br/><br/>{{.Popup.Times}}</div>')[0];
-	popup.setContent(html);
-	marker.bindPopup(popup);
-	`
+	var marker string
+	if m.Icon.MarkerColor == "random" {
+		col := randomColor()
+		marker = `
+		var marker = L.marker([{{.Position.Lat}}, {{.Position.Lon}}],{icon: new L.Icon.Default()}).addTo(marker_cluster);
+		var icon = L.AwesomeMarkers.icon({icon: '{{.Icon.Symbol}}', iconColor: '{{.Icon.IconColor}}', markerColor: '` + col + `', prefix: 'fa', extraClasses: 'fa-rotate-0'});
+		marker.setIcon(icon);
+	
+		var popup = L.popup({maxWidth: '300'});
+		var html = $('<div id="html" style="width: 100.0%%; height: 100.0%%;"><a target="_blank" href="{{.Popup.Link}}"><b>{{.Popup.Title}}</b></a><br/>{{.Popup.Desc}}<br/><br/><b>{{.Popup.Address}}</b><br/><br/>{{.Popup.Times}}</div>')[0];
+		popup.setContent(html);
+		marker.bindPopup(popup);`
+	} else {
+		marker = `
+		var marker = L.marker([{{.Position.Lat}}, {{.Position.Lon}}],{icon: new L.Icon.Default()}).addTo(marker_cluster);
+		var icon = L.AwesomeMarkers.icon({icon: '{{.Icon.Symbol}}', iconColor: '{{.Icon.IconColor}}', markerColor: '{{.Icon.MarkerColor}}', prefix: 'fa', extraClasses: 'fa-rotate-0'});
+		marker.setIcon(icon);
+	
+		var popup = L.popup({maxWidth: '300'});
+		var html = $('<div id="html" style="width: 100.0%%; height: 100.0%%;"><a target="_blank" href="{{.Popup.Link}}"><b>{{.Popup.Title}}</b></a><br/>{{.Popup.Desc}}<br/><br/><b>{{.Popup.Address}}</b><br/><br/>{{.Popup.Times}}</div>')[0];
+		popup.setContent(html);
+		marker.bindPopup(popup);`
+	}
 	tmpl, err := template.New("marker").Parse(marker)
 	if err != nil {
 		colorlog.Fatal(err.Error())
@@ -68,4 +82,9 @@ func MarkerString(m Marker, ma Mapdata) []byte {
 // Adds a marker to a map
 func AddMarker(m Marker, ma *Mapdata) {
 	ma.Markers = append(ma.Markers, m)
+}
+
+func randomColor() string {
+	rand.Seed(int64(time.Now().Nanosecond()))
+	return colors[rand.Perm(len(colors))[0]]
 }
